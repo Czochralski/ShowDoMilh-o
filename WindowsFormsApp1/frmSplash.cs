a148cf1153f857fb1157aef92fe26f4493d2b1b5
userblb <userblb@UNI002-L003-20.serlab.intra>
2018-05-09 09:18:46 -0300

    Primeiro envio

---
+++ WindowsFormsApp1/frmSplash.cs
@@ -0,0 +1,44 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace WindowsFormsApp1
+{
+    public partial class FrmSplash : Form
+    {
+        public FrmSplash()
+        {
+            InitializeComponent();
+        }
+
+        private void timer1_Tick(object sender, EventArgs e)
+        {
+
+            try
+            {
+
+                progressBar1.Value = progressBar1.Value + 1;
+
+            }
+            catch
+            {
+
+                timer1.Enabled = false;
+                this.Visible = false;
+                FrmNickName frm = new FrmNickName();
+                frm.ShowDialog();
+
+               
+            }
+
+            
+
+        }
+    }
+}
